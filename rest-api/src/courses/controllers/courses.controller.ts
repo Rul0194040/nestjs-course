ecd376ea0e032f9261b39b4eb70d2561369672a5
ruloxx <rsierravasconcelos@gmail.com>
2020-02-26 07:20:08 -0600

    terminada la implementacion del CRUD basaico, clase 17

--- rest-api/src/courses/controllers/courses.controller.ts
+++ rest-api/src/courses/controllers/courses.controller.ts
@@ -1,18 +1,39 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Put, Param, Body, Req, Res, Delete } from "@nestjs/common";
 import {Course} from "../../../../shared/course";
 import {findAllCourses} from "../../../db-data";
 import { CoursesRepository } from "../repositories/courses.repository";
+import { Request, Response } from "express";
 
-@Controller()
+@Controller("courses")
 export class CoursesController {
 
-  constructor(private coursesDB: CoursesRepository){
+  constructor(private coursesDB: CoursesRepository){}
 
+  @Put()
+  async createCourse(@Body() course: Partial<Course>): Promise<Course>{
+    console.log("creating course...");
+    return this.coursesDB.addCourse(course);
   }
 
-  @Get('api/courses')
+  @Get()
   async findAllCourses(): Promise<Course[]>{
+    console.log("showing courses...");
     return this.coursesDB.findAll();
+  }
+
+  @Put(':courseId')
+  async updateCourse(
+    @Param("courseId") courseId:string,
+    @Body() changes: Partial<Course>
+    ): Promise<Course> {
+    console.log("updating course...");
+    return this.coursesDB.updateCourse(courseId, changes);
+  }
+
+  @Delete(":courseId")
+  async deleteCourse(@Param("courseId") courseId:string){
+    console.log("deleting course...");
+    return this.coursesDB.deleteCourse(courseId);
   }
 
 
--- rest-api/src/courses/repositories/courses.repository.ts
+++ rest-api/src/courses/repositories/courses.repository.ts
@@ -6,12 +6,26 @@ import { InjectModel } from "@nestjs/mongoose";
 @Injectable()
 export class CoursesRepository{
 
-  constructor(@InjectModel("Course")
-              private courseModel: Model<Course>){
-
-  }
+  constructor(@InjectModel("Course") private courseModel: Model<Course>){}
 
   async findAll(): Promise<Course[]>{
     return this.courseModel.find();
+  }
+
+  async updateCourse(courseId: string, changes: Partial<Course>)
+  :Promise<Course> {
+    return this.courseModel.findOneAndUpdate(
+      {_id: courseId}, 
+      changes,
+      {new:true}
+      );
+  }
+  
+  deleteCourse(courseId: string){
+    return this.courseModel.deleteOne({_id:courseId});
+  }
+
+  addCourse(course: Partial<Course>): Promise<Course>{
+    return this.courseModel.create(course);
   }
 }
